5e470a26276032d5d7b4d46ee5d17a60ae447e1a
softdevstory <developer@softdevstory.net>
2015-04-07 17:12:23 +0900

    Trax with Mapkit is done.

---
+++ Trax/ImageViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ImageViewController.swift
+//  Cassini
+//
+//  Created by HS Song on 2015. 3. 17..
+//  Copyright (c) 2015년 softdevstory. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController, UIScrollViewDelegate {
+    
+    var imageURL: NSURL? {
+        didSet {
+            image = nil
+            if view.window != nil {
+                fetchImage()
+            }
+        }
+    }
+    
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
+    private func fetchImage() {
+        if let url = imageURL {
+            spinner?.startAnimating()
+            let qos = Int(QOS_CLASS_USER_INITIATED.value)
+            dispatch_async(dispatch_get_global_queue(qos, 0)) { () -> Void in
+                let imageData = NSData(contentsOfURL: url)
+                dispatch_async(dispatch_get_main_queue()) { () -> Void in
+                    if url == self.imageURL {
+                        if imageData != nil {
+                            self.image = UIImage(data: imageData!)
+                        } else {
+                            self.image = nil
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    @IBOutlet weak var scrollView: UIScrollView! {
+        didSet {
+            scrollView.contentSize = imageView.frame.size
+            scrollView.delegate = self
+            scrollView.minimumZoomScale = 0.03
+            scrollView.maximumZoomScale = 1.0
+        }
+    }
+    private var imageView = UIImageView()
+    
+    private var image: UIImage? {
+        get { return imageView.image }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+            scrollView?.contentSize = imageView.frame.size
+            spinner?.stopAnimating()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scrollView.addSubview(imageView)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if image == nil {
+            fetchImage()
+        }
+    }
+}
---
+++ Trax/MKGPX.swift
@@ -0,0 +1,39 @@
+//
+//  MKGPX.swift
+//  Trax
+//
+//  Created by HS Song on 2015. 4. 7..
+//  Copyright (c) 2015년 softdevstory. All rights reserved.
+//
+
+import MapKit
+
+extension GPX.Waypoint: MKAnnotation {
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    var title: String! {
+        return name
+    }
+    
+    var subtitle: String! {
+        return info
+    }
+ 
+    var thumbnailURL: NSURL? {
+        return getImageURLofType("thumbnail")
+    }
+    var imageURL: NSURL? {
+        return getImageURLofType("large")
+    }
+    
+    private func getImageURLofType(type: String) -> NSURL? {
+        for link in links {
+            if link.type == type {
+                return link.url
+            }
+        }
+        return nil
+    }
+}
